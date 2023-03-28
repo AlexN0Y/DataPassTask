7719316f8a92c5c9b521a6b3de93337c91f257ed
Alex <gao.sanya@gmail.com>
2023-03-28 14:09:31 +0300

    Initial Commit

---
+++ 1stTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1stTask
+//
+//  Created by Alex Gav on 28.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
